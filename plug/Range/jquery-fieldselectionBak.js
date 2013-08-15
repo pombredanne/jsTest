88b27b780031d53466c3a0c05ef5761f221e2fae
rambo-panda <6291216li>
2013-08-15 18:35:09 +0800

    Range

---
+++ plug/Range/jquery-fieldselectionBak.js
@@ -0,0 +1,257 @@
+/*
+ https://github.com/localhost
+ 主要用于:
+ input textarea 可編輯div  的光标位置 以及选取内容
+ getSelection  針對div 是強制使用trim  input 則可以進行調節
+ */
+
+(function() {
+
+    "use strict";
+
+    var fieldSelection = {
+
+        getSelection: function(opts) {
+            opts=opts||{};
+            var dom = this.jquery ? this[0] : this,
+                isDiv= dom.tagName.toUpperCase()==='DIV'&& dom.contentEditable === "true",
+                getFirstStr=function(dom){
+                        var first=0,
+                            firstDom=dom.firstChild||{data:dom.value};
+                        if(firstDom.data.search(/\s/)>-1){    //为了防止eg:<TextNode textContent="\n   this is a test\n   ">]  nodeType=3   <div>  nodeType=1  注释：8
+                            //dom.firstChild.textContent=dom.firstChild.textContent.trimLeft();
+                            first=firstDom.textContent.search($.trim(firstDom.textContent).slice(0,1))||0;  //TODO 让得出结果减去它即可
+                        }
+                };
+
+            return (
+                (dom.selectionStart && function() {  // dom 3.0
+                    var length = dom.selectionEnd - dom.selectionStart,
+                        first=opts.trim?getFirstStr(dom)||0;
+                    return { start: dom.selectionStart-first, end: dom.selectionEnd-first, length: length, text: dom.value.substr(dom.selectionStart, l) };
+                }) ||(isDiv&&function(){    //获取div的信息
+                        var getPositionresult = 0,
+                            eleText=dom.textContent?dom.textContent:dom.innerText;  //TODO chrome 支持innerText 并且效果=== dom.textContent.trim();
+                        if(window.getSelection) { //非IE浏览器||>ie8
+                            var getSel=window.getSelection(),
+                                first=getFirstStr(dom),
+                                start,
+                                result={
+                                    value:function(){
+                                        try{
+                                            return getSel.toString?getSel.toString():document.selection.createRange().text;
+                                        }catch(e){
+                                            return ''
+                                        }
+                                    },
+                                    length:function(){
+                                        return this.value();
+                                    },
+                                    end:function(start){
+                                        return this.value()?start+this.length():start;
+                                    }
+                                };
+
+                            if(getSel.anchorNode.textContent === eleText) {
+                                start=getSel.anchorOffset;
+                                return {start:start-(first-0),end:result.end(start),value:result.value(),length:result.length(),trim:first};
+                            } else {
+                                var currentIndex = getSel.anchorOffset,
+                                    txt = "",
+                                    txtoo = getSel.anchorNode.previousSibling;
+                                while(txtoo != null) {
+                                    txt += txtoo.textContent;
+                                    txtoo = txtoo.previousSibling;
+                                }
+                                start = txt.trim().length + currentIndex;
+                                return {start:start,end:result.end(start),value:result.value(),length:result.length(),trim:first};
+                            }
+                        }else{  //<ie9的
+                            dom.focus();
+                            var first=getFirstStr(dom),
+                                Sel = document.selection.createRange(),
+                                Sel2 = Sel.duplicate(),
+                                CaretPos = -1;
+
+                            Sel2.moveToElementText(dom);
+                            while (Sel2.inRange(Sel)) {
+                                Sel2.moveStart('character');
+                                CaretPos++;
+                            }
+                            var text=Sel.text,
+                                end=text.length?CaretPos-first+text.length:start;
+                            return {start:CaretPos-first,end:end,value:text,length:text.length,trim:first};
+                        }
+                    })||
+                    (document.selection && function() {     //针对<ie9
+
+                        dom.focus();
+                        var r = document.selection.createRange();
+                            first=opts.trim?getFirstStr(dom)||0;
+
+                        if(r == null) {
+                            return { start: 0, end: dom.value.length-first,text:'', length: 0 }
+                        }
+
+                        var re = dom.createTextRange(),
+                            rc = re.duplicate();
+                        re.moveToBookmark(r.getBookmark());
+                        rc.setEndPoint('EndToStart', re);
+
+                        return { start: rc.text.length-start, end: rc.text.length + r.text.length-first, length: r.text.length, text: r.text };
+                    })||
+                    function() {  //browser not supported
+                        return { start: 0, end: dom.value.length, length: 0 };
+                    }
+                )();
+        },
+
+        replaceSelection: function(text) {
+
+            var dom = this.jquery ? this[0] : this,
+                isDiv= dom.tagName.toUpperCase()==='DIV'&& dom.contentEditable === "true";
+            text = text || '';
+
+            return (
+
+                /* mozilla / dom 3.0 */
+                ('selectionStart' in dom && function() {
+                    dom.value = dom.value.substr(0, dom.selectionStart) + text + dom.value.substr(dom.selectionEnd, dom.value.length);
+                    return this;
+                }) ||
+
+                    /* exploder */
+                    (document.selection && function() {
+                        dom.focus();
+                        document.selection.createRange().text = text;
+                        return this;
+                    }) ||
+
+                    /*div的取值*/
+                    (isDiv&&function() {
+                        var selection = window.getSelection ? window.getSelection() : document.selection;
+                        var range = selection.createRange ? selection.createRange() : selection.getRangeAt(0);
+                        if(!window.getSelection) { //IE
+                            dom.focus();
+                            range.pasteHTML(text);
+                            range.collapse(false);
+                            range.select();
+                        } else {
+                            try{
+                                dom.focus();
+                            }catch(e){
+                                // $(dom).focus();
+                            }
+                            range.collapse(false); //将插入点移动到当前范围的开始或结尾。
+                            var hasR = range.createContextualFragment(text);
+                            var hasR_lastChild = hasR.lastChild;
+                            while(hasR_lastChild && hasR_lastChild.nodeName.toLowerCase() == "br" && hasR_lastChild.previousSibling && hasR_lastChild.previousSibling.nodeName.toLowerCase() == "br") {
+                                var dom = hasR_lastChild;
+                                hasR_lastChild = hasR_lastChild.previousSibling;
+                                hasR.removeChild(dom);
+                            }
+                            range.insertNode(hasR);
+                            if(hasR_lastChild) {
+                                range.setEndAfter(hasR_lastChild);
+                                range.setStartAfter(hasR_lastChild);
+                            }
+                        }
+                        selection.removeAllRanges();
+                        selection.addRange(range);
+                    })||
+
+                    /* browser not supported */
+                    function() {
+                        dom.value += text;
+                        return this;
+                    }
+
+                )();
+        },
+        setCursorPosition:function(pos,end,start){
+            pos=(pos-0)||0;
+            var dom = this.jquery ? this[0] : this,
+                isDiv= dom.tagName==='DIV'&& dom.contentEditable === "true";
+            end&&(pos=dom.value.length);
+            if (dom.setSelectionRange) {
+                dom.focus();
+                dom.setSelectionRange(pos, pos);
+                //dom.selectionStart=pos
+            }else if (dom.createTextRange) {
+                var range = dom.createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }else if(isDiv&&window.getSelection){
+                var first,
+                    _first;
+                if(start||end){
+                    first=end?dom.lastChild:dom.firstChild;
+                    _first=end? dom.lastChild.nodeType===3?dom.lastChild.length:1:0;
+                }else{
+                    var parents=dom.parentNode,
+                        text='',
+                        length2= 0,
+                        innerText=$.trim(dom.textContent||dom.innerText),
+                        childs=parents.childNodes;
+
+                    first=dom.firstChild;
+                    _first=0;
+                    var getFirstNode=function(first){
+                        if(first.nodeType===3&&!first.data.trim()){
+                            var length= 0,
+                                getLen=function(dom){
+                                    for(var i=0,len=dom.childNodes.length;i<len;i++){
+                                        var cache=dom.childNodes[i];
+                                        var data=$.trim(cache.data||(cache.firstChild.data?cache.firstChild.data:cache.firstChild.textContent));
+                                        var t=0;
+                                        if(cache.childNodes.length){
+                                            var caLen=cache.childNodes.length;
+                                            while(true){
+                                                var Ccache=cache.childNodes[t];
+                                                var data=$.trim(Ccache.data||(Ccache.firstChild.data?Ccache.firstChild.data:Ccache.firstChild.textContent));
+                                                length=length+data.length;
+                                                if(length>pos){
+                                                    break;
+                                                }
+                                                length2=length;
+                                                t++;
+                                            }
+                                        }else{
+                                            length=length+data.length;
+                                        }
+
+                                        if(length>pos){
+                                            return t>0?cache.childNodes[t]:cache;
+                                        }
+                                        length2=length;
+                                    }
+                                };
+                            var cache=getLen(dom);
+                            if(cache.nodeType!==3&&!cache.data){
+                                return cache.firstChild;
+                            }
+                        }else{
+                            return first;
+                        }
+                    };
+
+                    first=getFirstNode(first);
+                    if((first.data.search(/\n/)>-1||first.data.search(' ')>-1)){
+                        text=first.textContent;
+                        _first=text.search(jQuery.trim(text).slice(0,1));
+                    }
+                    _first=length2<pos?pos-length2:(_first+pos);
+                }
+                var sel = window.getSelection(),
+                    tempRange = document.createRange();
+                tempRange.setStart(first, _first);
+                sel.removeAllRanges();
+                sel.addRange(tempRange);
+            }
+        }
+    };
+
+    jQuery.each(fieldSelection, function(i) { jQuery.fn[i] = this; });
+})();
--- selfDocument/hashtable.js
+++ selfDocument/hashtable.js
@@ -1,4 +1,4 @@
-function Hashtable(){
+function Hashtable() {
     this.clear = hashtable_clear;
     this.containsKey = hashtable_containsKey;
     this.containsValue = hashtable_containsValue;
@@ -11,25 +11,23 @@ function Hashtable(){
     this.toString = hashtable_toString;
     this.values = hashtable_values;
     //this.hashtable = new Array();
-    this.hashtable=[];
+    this.hashtable = [];
 }
 
-
-function hashtable_clear(){             //清空
-   this.hashtable=[];
+function hashtable_clear() { //清空
+    this.hashtable = [];
 }
 
-function hashtable_containsKey(key){    //key包含条件key
-    
-	for (var i in this.hashtable) {
+function hashtable_containsKey(key) { //key包含条件key
+    for (var i in this.hashtable) {
         if (i === key && this.hashtable[i] != null) {
-			 return true;
+            return true;
         }
     }
     return false;
 }
 
-function hashtable_containsValue(value){    //value包含条件
+function hashtable_containsValue(value) { //value包含条件
     if (valule != null) {
         for (var i in this.hashtable) {
             if (this.hashtable[i] == value) {
@@ -40,28 +38,28 @@ function hashtable_containsValue(value){    //value包含条件
     return false;
 }
 
-function hashtable_get(key){        //get by key
+function hashtable_get(key) { //get by key
     return this.hashtable[key];
 }
 
 /*判断是否为空*/
-function hashtable_isEmpty(){
-    return (this.size === 0) ? true : false;
+function hashtable_isEmpty() {
+    return (this.size === 0) ? true: false;
 }
 
 /*将key存入一个数组*/
-function hashtable_keys(){
+function hashtable_keys() {
     var keys = new Array();
     for (var i in this.hashtable) {
-        if (this.hashtable[i] != null){
-			 keys.push(i);
-		 }            
+        if (this.hashtable[i] != null) {
+            keys.push(i);
+        }
     }
     return keys;
 }
 
 /*将value存入一个数组*/
-function hashtable_values(){
+function hashtable_values() {
     var values = new Array();
     for (var i in this.hashtable) {
         if (this.hashtable[i] != null) {
@@ -72,80 +70,80 @@ function hashtable_values(){
 }
 
 /*存value*/
-function hashtable_put(key, value){
-    if(argument.length===1 && Object.prototype.toString.call(argument[0])==="[object Array]"){
-        for(var i=-1,val;val=key[i++];){
-           this.hashtable[val[0]]=val[1];
+function hashtable_put(key, value) {
+    if (argument.length === 1 && Object.prototype.toString.call(argument[0]) === "[object Array]") {
+        for (var i = - 1, val; val = key[i++];) {
+            this.hashtable[val[0]] = val[1];
         }
     }
-    if (!key || !value) {
+    if (!key || ! value) {
         throw 'NullPointerException{' + key + '},{' + value + '}';
-    }else {
+    } else {
         this.hashtable[key] = value
     }
 }
 
 /*移除*/
-function hashtable_remove(key){
+function hashtable_remove(key) {
     var rtn = this.hashtable[key];
     this.hahstable.splice(key, 1);
     return rtn;
 }
 
 /*长度*/
-function hashtable_size(isnull){
-	if(isnull){
-		//返回实际长度
-		return this.hashtable.length;   		
-	}
-	//返回有数据的长度
+function hashtable_size(isnull) {
+    if (isnull) {
+        //返回实际长度
+        return this.hashtable.length;
+    }
+    //返回有数据的长度
     var size = 0;
     for (var i in this.hashtable) {
         if (this.hashtable[i] != null) {
             size++;
         }
-        
+
     }
     return size;
-	
+
 }
 
 /*转换string*/
-function hashtable_toString(debug){
-    var result =[];
+function hashtable_toString(debug) {
+    var result = [];
     for (var i in this.hashtable) {
-        var param=this.hashtable[i];
+        var param = this.hashtable[i];
         if (this.hashtable[i] != null) {
-            
-            if(typeof param==='object'){
-                result.push(i+':'+zhun(this.hashtable[i]))
-            }else{      
-                result.push(i+':'+this.hashtable[i]);
+
+            if (typeof param === 'object') {
+                result.push(i + ':' + zhun(this.hashtable[i]))
+            } else {
+                result.push(i + ':' + this.hashtable[i]);
             }
             //result.push(i+':'+this.hashtable[i])
         }
     }
-    if(debug){
+    if (debug) {
         return result.join('\n'); //replace(/\n/g,'')   
-    }else{
-        return result.join('');    
+    } else {
+        return result.join('');
     }
-    
+
 }
 
-function zhun(object){
-    var cache=[];
-    for(var i in object){
-        var ca=object[i];
-        if(ca){
-            if(typeof ca==='object'){
-                cache.push(i+' : '+zhun(ca))
-            }else{
-                cache.push(i+' : '+ca);
+function zhun(object) {
+    var cache = [];
+    for (var i in object) {
+        var ca = object[i];
+        if (ca) {
+            if (typeof ca === 'object') {
+                cache.push(i + ' : ' + zhun(ca))
+            } else {
+                cache.push(i + ' : ' + ca);
             }
-        }        
+        }
     }
-    return '{'+ cache.join() +'}'
+    return '{' + cache.join() + '}'
 }
 /*
 
@@ -161,14 +159,14 @@ function zhun(object){
 
  //遍历hashtable 严格遍历(不推荐使用) 个人推荐使用第二种hashtable方法进行遍历 
 for (var i = 0, a; a = hashTable.hashtable[i++];) {
-	console.log(a);
+    console.log(a);
 }
 
  //遍历hashtable  用hashtable方法
 
- for(var key in hashTable.hashtable){	
-	console.log(hashTable.get(key));	
- }	
+ for(var key in hashTable.hashtable){   
+    console.log(hashTable.get(key));    
+ }  
 
 
  alert(hashTable.containsKey(1));  //返回true
@@ -182,6 +180,4 @@ for (var i = 0, a; a = hashTable.hashtable[i++];) {
  alert(hashTable.containsKey(1));  //因为key为1的元素已被上行的remove移除，所以返回false
 
  */
-
-
 
